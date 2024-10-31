accbe705c3681f4020f8fcf9c4f6b30f8b7d5ffc
leaf_windows <zxy_yys_leaf@163.com>
2024-10-31 13:30:54 +0800

    2024/10/31 Important update on fast_postprocess

--- yolov5_demo.py
+++ yolov5_demo.py
@@ -7,20 +7,19 @@
 # ==================================================
 import numpy as np
 from hobot_dnn import pyeasy_dnn as dnn
-import fast_postprocess.postprocess as pprcoc
+from fast_postprocess import RDKyolov5postprocess
 import bpu_yolov5_tools as tools
 import argparse, yaml, time, cv2, os
 
 if __name__ == '__main__':
     # 参数设置
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, default='./models/yolov5s_672x672_nv12.bin', help="bin模型路径")
+    parser.add_argument('--model', type=str, default='/app/model/basic/yolov5s_672x672_nv12.bin', help="bin模型路径")
     parser.add_argument('--image', type=str, default='./images/kite.jpg', help='推理图片路径')
     parser.add_argument('--names', type=str, default='./models/demo.yaml', help='name列表yaml文件路径')
     parser.add_argument('--sco_thr', type=float, default=0.4, help='得分阈值')
     parser.add_argument('--nms_thr', type=float, default=0.45, help='NMS阈值')
-    parser.add_argument('--swap_num', type=int, default=64, help='中间数据长度')
-    parser.add_argument('--max_num', type=int, default=32, help='输出结果的最大值')
+    parser.add_argument('--thread_num', type=int, default=8, help='后处理线程数')
     parser.add_argument('--nice', type=int, default=0, help='程序优先级')
     parser.add_argument('--CPUOC', type=bool, default=False, help='是否CPU超频')
     opt = parser.parse_args()
@@ -37,7 +36,6 @@ if __name__ == '__main__':
 
         # 加载模型
         models = dnn.load(opt.model)
-
 
         # cv2加载图片
         img = cv2.imread(opt.image) 
@@ -57,6 +55,13 @@ if __name__ == '__main__':
         model_size = models[0].inputs[0].properties.shape[2]
         class_number = models[0].outputs[0].properties.shape[-1]//3-5
 
+        # 后处理工具
+        pst = RDKyolov5postprocess.yolov5postprocess(
+            model_size, 
+            class_number, 
+            score_threshold = opt.sco_thr, 
+            nms_threshold = opt.nms_thr, 
+            thread_num = opt.thread_num)
 
         # 图片格式转换
         h, w = models[0].inputs[0].properties.shape[2:]
@@ -71,18 +76,15 @@ if __name__ == '__main__':
 
         # 后处理
         # 如果需要测试后处理1k次，可以把下方注释解除
-        # for i in range(999):
-        #     results = pprcoc.postprocess(outputs, model_size, class_number, img.shape[:2])
-        results = pprcoc.postprocess(
-            outputs, 
-            model_size, 
-            class_number, 
-            img.shape[:2], 
-            score_threshold = opt.sco_thr, 
-            nms_threshold = opt.nms_thr, 
-            swap_num = opt.swap_num, 
-            max_num = opt.max_num)
+        for i in range(1000):
+            results = pst.process(outputs)
+        # results = pst.process(outputs)
         t2 = time.time()
+
+        results[:,0] *= img.shape[1]
+        results[:,1] *= img.shape[0]
+        results[:,2] *= img.shape[1]
+        results[:,3] *= img.shape[0]
 
         # 输出结果
         tools.result_show(results, names['names'])
